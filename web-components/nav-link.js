2b10dff16190b6cf71ff5ec07c021548cac94f3e
Alex Kruckenberg <alexa.kruckenberg@gmail.com>
2026-03-05 01:27:52 +0000

    fiddling with clip paths

--- web-components/nav-link.js
+++ web-components/nav-link.js
@@ -2,7 +2,7 @@ class NavLink extends HTMLElement {
 
     constructor() {
         super();
-        this.text = 'Link';
+        this.text = 'Nav Link';
     }
 
     // component attributes
@@ -12,7 +12,6 @@ class NavLink extends HTMLElement {
 
     // attribute change
     attributeChangedCallback(property, oldValue, newValue) {
-
         if (oldValue === newValue) return;
         this[property] = newValue;
 
@@ -21,18 +20,37 @@ class NavLink extends HTMLElement {
     // connect component
     connectedCallback() {
         this.innerHTML = `
-        <a>
-            ${this.text}
-        </a>
-        <style>
-            a {
-                line-height: 0;
-            }            
-            a:hover {
-                font-size: large;
-                line-height: 0;
-            }
-      </style>`;
+            <div class="nav-link-container">
+                <a class="nav-link">${this.text}</a>
+                <svg class="paint-splotch" xmlns="http://www.w3.org/2000/svg" height="75px" width="75px">
+                    <rect height="75px" width="75px" />
+                </svg>
+            </div>
+            <style>
+                .nav-link-container {
+                    position: relative;
+                }
+                .nav-link-container:hover .paint-splotch {
+                    display: block;
+                }
+                .nav-link-container:hover .nav-link {
+                    font-size: large;
+                    line-height: 0;
+                }
+                .nav-link {
+                    line-height: 0;
+                }
+                .paint-splotch {
+                    display: none;
+                    position: absolute;
+                    top: -25px;
+                    left: -5px;
+                }
+                .paint-splotch rect {
+                    clip-path: url('./public/paint-splotch.svg#clip');
+                }
+            </style>
+        `;
     }
 
 }
