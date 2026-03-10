f9a5bbb34062558ee1e789376aaf642c89a09059
Alex Kruckenberg <alexa.kruckenberg@gmail.com>
2026-03-10 23:00:27 +0000

    paint splotches -> music notes

--- web-components/nav-link.js
+++ web-components/nav-link.js
@@ -1,28 +1,32 @@
 class NavLink extends HTMLElement {
-  constructor() {
-    super();
-    this.text = "Nav Link";
-    this.splotchcolor = "black";
-    this.shadow = this.attachShadow({ mode: "open" });
-  }
+    constructor() {
+        super();
+        this.text = "Nav Link";
+        this.splotchcolor = "black";
+        this.shadow = this.attachShadow({ mode: "open" });
+    }
 
-  // component attributes
-  static get observedAttributes() {
-    return ["text", "splotchcolor"];
-  }
+    // component attributes
+    static get observedAttributes() {
+        return ["text", "splotchcolor"];
+    }
 
-  // attribute change
-  attributeChangedCallback(property, oldValue, newValue) {
-    if (oldValue === newValue) return;
-    this[property] = newValue;
-  }
+    // attribute change
+    attributeChangedCallback(property, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        this[property] = newValue;
+    }
 
-  // connect component
-  connectedCallback() {
-    this.shadow.innerHTML = `
+    // connect component
+    connectedCallback() {
+        this.shadow.innerHTML = `
             <div class="nav-link-container">
                 <a class="nav-link">${this.text}</a>
-                <div class="paint-splotch-${this.text}"></div>
+                <svg class="music-notes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 595.3 841.9">
+                    <use href="./public/images/svg/music-notes.svg#eighth-note" class="note-1"/>
+                    <use href="./public/images/svg/music-notes.svg#quarter-note" class="note-2"/>
+                    <use href="./public/images/svg/music-notes.svg#double-eighth-note" class="note-3"/>
+                </svg>
             </div>
             <style>
                 .nav-link-container {
@@ -33,6 +37,9 @@ class NavLink extends HTMLElement {
                     align-items: center;
                 }
                 .nav-link-container:hover .paint-splotch-${this.text} {
+                    opacity: 1;
+                }
+                .nav-link-container:hover .music-notes {
                     opacity: 1;
                 }
                 .nav-link {
@@ -65,9 +72,37 @@ class NavLink extends HTMLElement {
                         width: 50px;
                     }
                 }
+                .music-notes {
+                    opacity: 0;
+                    position: absolute;
+                    height: 100px;
+                    width: 150px;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    z-index: 0;
+                }
+                .note-1 {
+                    scale: 200%;
+                    rotate: -20deg;
+                    translate: 40px 230px;
+                    transform-box: fill-box;
+                }
+                .note-2 {
+                    scale: 200%;
+                    rotate: 30deg;
+                    translate: -350px -280px;
+                    transform-box: fill-box;
+                }
+                .note-3 {
+                    scale: 200%;
+                    rotate: 30deg;
+                    translate: 460px -190px;
+                    transform-box: fill-box;
+                }
             </style>
         `;
-  }
+    }
 }
 
 // register component
