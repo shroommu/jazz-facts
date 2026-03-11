c780c0b58a793b996f46891923dd878c6e0c8485
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-03-10 21:12:17 -0700

    cleanup

--- web-components/nav-link.js
+++ web-components/nav-link.js
@@ -2,13 +2,12 @@ class NavLink extends HTMLElement {
   constructor() {
     super();
     this.text = "Nav Link";
-    this.splotchcolor = "black";
     this.shadow = this.attachShadow({ mode: "open" });
   }
 
   // component attributes
   static get observedAttributes() {
-    return ["text", "splotchcolor"];
+    return ["text"];
   }
 
   // attribute change
@@ -20,18 +19,18 @@ class NavLink extends HTMLElement {
   // connect component
   connectedCallback() {
     this.shadow.innerHTML = `
-            <div class="nav-link-container">
-                <a class="nav-link">${this.text}</a>
-                <svg class="music-notes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 595.3 841.9">
-                    <use href="./public/images/svg/music-notes.svg#eighth-note" class="note-1"/>
-                    <use href="./public/images/svg/music-notes.svg#quarter-note" class="note-2"/>
-                    <use href="./public/images/svg/music-notes.svg#double-eighth-note" class="note-3"/>
-                </svg>
-            </div>
-            <style>
-                @import "./web-components/nav-link.css";
-            </style>
-        `;
+        <div class="nav-link-container">
+            <a class="nav-link">${this.text}</a>
+            <svg class="music-notes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 595.3 841.9">
+                <use href="./public/images/svg/music-notes.svg#eighth-note" class="note-1"/>
+                <use href="./public/images/svg/music-notes.svg#quarter-note" class="note-2"/>
+                <use href="./public/images/svg/music-notes.svg#double-eighth-note" class="note-3"/>
+            </svg>
+        </div>
+        <style>
+            @import "./web-components/nav-link.css";
+        </style>
+    `;
   }
 }
 
