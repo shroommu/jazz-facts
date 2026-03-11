eaf88b56d53a51783625d9b3b5a8b65345c49142
Alex Kruckenberg <alexa.kruckenberg@gmail.com>
2026-03-11 21:23:25 +0000

    record album -> own file

---
+++ web-components/record-album.js
@@ -0,0 +1,37 @@
+import "./nav-link.js";
+
+const heroImageUrl = new URL(
+    "../public/images/avif/jazz-headliner.avif",
+    import.meta.url,
+).href;
+const recordAlbumCssUrl = new URL("./record-album.css", import.meta.url).href;
+
+class RecordAlbum extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    // connect component
+    connectedCallback() {
+        this.innerHTML = `
+        <link rel="preload" as="image"
+        href="${heroImageUrl}" />
+        <a class="album-cover-link" href="/playlist/index.html">
+        <div class="album-cover">
+            <img class="hero-image" src="/public/images/avif/jazz-headliner.avif" loading="eager"
+                fetchpriority="high" />
+            <svg class="animated-record" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 10 10" height="100%"
+                width="100%">
+                <circle cx="5" cy="5" r="5"></circle>
+            </svg>
+        </div>
+    </a>
+        <style>
+        @import "${recordAlbumCssUrl}";
+        </style>
+    `;
+    }
+}
+
+// register component
+customElements.define("record-album", RecordAlbum);
