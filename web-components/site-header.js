e9fe4065de8fc562f88521dfd59f446fe216b06d
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-03-10 21:33:00 -0700

    style changes

--- web-components/site-header.js
+++ web-components/site-header.js
@@ -11,9 +11,9 @@ class SiteHeader extends HTMLElement {
         <link rel="preload" as="image"
             href="./public/images/svg/music-notes.svg" />
         <header>
+            <a class="site-title" href="/">Jazz Facts</a>
             <nav>
                 <ul class="nav-item-list">
-                    <li><nav-link text="Overview" href="./overview/index.html"></nav-link></li>
                     <li><nav-link text="History" href="./history/index.html"></nav-link></li>
                     <li><nav-link text="Genres" href="./genres/index.html"></nav-link></li>
                     <li><nav-link text="Playlist" href="./playlist/index.html"></nav-link></li>
