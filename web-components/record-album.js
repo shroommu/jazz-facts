36c77693deb9788c155d90fc467ba64d35e328ad
Alex Kruckenberg <alexa.kruckenberg@gmail.com>
2026-03-11 22:47:33 +0000

    add title text

--- web-components/record-album.js
+++ web-components/record-album.js
@@ -20,6 +20,8 @@ class RecordAlbum extends HTMLElement {
         <div class="album-cover">
             <img class="hero-image" src="/public/images/avif/jazz-headliner.avif" loading="eager"
                 fetchpriority="high" />
+            <div class="album-cover-title">Top Hits</div>
+            <div class="album-cover-subtitle">From All The Greats</div>
             <svg class="animated-record" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 10 10" height="100%"
                 width="100%">
                 <circle cx="5" cy="5" r="5"></circle>
