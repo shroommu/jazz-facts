8c11a54e1138aff49c7d8e140503743d70e0333f
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-03-10 20:16:09 -0700

    break out css into its own file

--- web-components/nav-link.js
+++ web-components/nav-link.js
@@ -1,25 +1,25 @@
 class NavLink extends HTMLElement {
-    constructor() {
-        super();
-        this.text = "Nav Link";
-        this.splotchcolor = "black";
-        this.shadow = this.attachShadow({ mode: "open" });
-    }
+  constructor() {
+    super();
+    this.text = "Nav Link";
+    this.splotchcolor = "black";
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
 
-    // component attributes
-    static get observedAttributes() {
-        return ["text", "splotchcolor"];
-    }
+  // component attributes
+  static get observedAttributes() {
+    return ["text", "splotchcolor"];
+  }
 
-    // attribute change
-    attributeChangedCallback(property, oldValue, newValue) {
-        if (oldValue === newValue) return;
-        this[property] = newValue;
-    }
+  // attribute change
+  attributeChangedCallback(property, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this[property] = newValue;
+  }
 
-    // connect component
-    connectedCallback() {
-        this.shadow.innerHTML = `
+  // connect component
+  connectedCallback() {
+    this.shadow.innerHTML = `
             <div class="nav-link-container">
                 <a class="nav-link">${this.text}</a>
                 <svg class="music-notes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 595.3 841.9">
@@ -29,62 +29,10 @@ class NavLink extends HTMLElement {
                 </svg>
             </div>
             <style>
-                .nav-link-container {
-                    position: relative;
-                    display: flex;
-                    justify-items: center;
-                    align-items: center;
-                }
-                .nav-link-container:hover .music-notes {
-                    opacity: 1;
-                }
-                .nav-link {
-                    font-family: "Limelight";
-                    font-size: x-large;
-                    z-index: 10;
-                }
-                @media (max-width: 800px) {
-                    .nav-link {
-                        font-size: medium;
-                    }
-                }
-                .music-notes {
-                    opacity: 0;
-                    position: absolute;
-                    height: 100px;
-                    width: 150px;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
-                    z-index: 0;
-                }
-                .note-1 {
-                    scale: 200%;
-                    rotate: -20deg;
-                    translate: 40px 230px;
-                    transform-box: fill-box;
-                }
-                .note-2 {
-                    scale: 200%;
-                    rotate: 30deg;
-                    translate: -350px -280px;
-                    transform-box: fill-box;
-                }
-                .note-3 {
-                    scale: 200%;
-                    rotate: 30deg;
-                    translate: 460px -190px;
-                    transform-box: fill-box;
-                }
-                @media (max-width: 800px) {
-                    .music-notes {
-                        height: 75px;
-                        width: 112.5px;
-                    }
-                }
+                @import "./web-components/nav-link.css";
             </style>
         `;
-    }
+  }
 }
 
 // register component
