9bff10cb208f1e9a717ba336c3d363d282bf799c
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-03-10 21:47:46 -0700

    fix imports again

--- web-components/nav-link.js
+++ web-components/nav-link.js
@@ -19,17 +19,23 @@ class NavLink extends HTMLElement {
 
   // connect component
   connectedCallback() {
+    const musicNotesSpriteUrl = new URL(
+      "../public/images/svg/music-notes.svg",
+      import.meta.url
+    ).href;
+    const navLinkCssUrl = new URL("./nav-link.css", import.meta.url).href;
+
     this.shadow.innerHTML = `
         <div class="nav-link-container">
             <a class="nav-link" href="${this.href}">${this.text}</a>
             <svg class="music-notes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 595.3 841.9">
-                <use href="./public/images/svg/music-notes.svg#eighth-note" class="note-1"/>
-                <use href="./public/images/svg/music-notes.svg#quarter-note" class="note-2"/>
-                <use href="./public/images/svg/music-notes.svg#double-eighth-note" class="note-3"/>
+                <use href="${musicNotesSpriteUrl}#eighth-note" class="note-1"/>
+                <use href="${musicNotesSpriteUrl}#quarter-note" class="note-2"/>
+                <use href="${musicNotesSpriteUrl}#double-eighth-note" class="note-3"/>
             </svg>
         </div>
         <style>
-            @import "./web-components/nav-link.css";
+            @import "${navLinkCssUrl}";
         </style>
     `;
   }
--- web-components/site-header.js
+++ web-components/site-header.js
@@ -1,5 +1,15 @@
 import "./nav-link.js";
 
+const musicNotesSpriteUrl = new URL(
+  "../public/images/svg/music-notes.svg",
+  import.meta.url
+).href;
+const siteHeaderCssUrl = new URL("./site-header.css", import.meta.url).href;
+const homePageUrl = new URL("../index.html", import.meta.url).href;
+const historyPageUrl = new URL("../history/index.html", import.meta.url).href;
+const genresPageUrl = new URL("../genres/index.html", import.meta.url).href;
+const playlistPageUrl = new URL("../playlist/index.html", import.meta.url).href;
+
 class SiteHeader extends HTMLElement {
   constructor() {
     super();
@@ -9,19 +19,19 @@ class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <link rel="preload" as="image"
-            href="./public/images/svg/music-notes.svg" />
+        href="${musicNotesSpriteUrl}" />
         <header>
-            <a class="site-title" href="/">Jazz Facts</a>
+        <a class="site-title" href="${homePageUrl}">Jazz Facts</a>
             <nav>
                 <ul class="nav-item-list">
-                    <li><nav-link text="History" href="./history/index.html"></nav-link></li>
-                    <li><nav-link text="Genres" href="./genres/index.html"></nav-link></li>
-                    <li><nav-link text="Playlist" href="./playlist/index.html"></nav-link></li>
+            <li><nav-link text="History" href="${historyPageUrl}"></nav-link></li>
+            <li><nav-link text="Genres" href="${genresPageUrl}"></nav-link></li>
+            <li><nav-link text="Playlist" href="${playlistPageUrl}"></nav-link></li>
                 </ul>
             </nav>
         </header>
         <style>
-            @import "./web-components/site-header.css";
+        @import "${siteHeaderCssUrl}";
         </style>
     `;
   }
